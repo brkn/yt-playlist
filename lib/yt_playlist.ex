93e37fe00778f272fad51e89d8dda9ff851d2bde
Berkan Unal <Berkanunal@gmail.com>
2025-12-15 19:36:15 +0300

    mix new . --app yt_playlist

---
+++ lib/yt_playlist.ex
@@ -0,0 +1,18 @@
+defmodule YtPlaylist do
+  @moduledoc """
+  Documentation for `YtPlaylist`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> YtPlaylist.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/yt_playlist_test.exs
@@ -0,0 +1,8 @@
+defmodule YtPlaylistTest do
+  use ExUnit.Case
+  doctest YtPlaylist
+
+  test "greets the world" do
+    assert YtPlaylist.hello() == :world
+  end
+end
